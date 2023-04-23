835183aa7f5e84f7fabeb56cb69bee6d82d4b27c
TKMhub <111867865+TKMhub@users.noreply.github.com>
2023-04-23 23:04:16 +0900

    0423_home

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -4,7 +4,6 @@ import { Header } from "components/Header/Header";
 const Layout = ({ children }) => {
   return (
     <>
-      {/* 共通のヘッダーなどを実装 */}
       <Header />
       <div className="menu"></div>
       <main>{children}</main>
