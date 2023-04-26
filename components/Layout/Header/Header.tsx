318ab48521aa6af754390480a0ea5b8461c06e62
TKMhub <111867865+TKMhub@users.noreply.github.com>
2023-04-26 13:04:40 +0900

    0426_ofiice

--- components/Layout/Header/Header.tsx
+++ components/Layout/Header/Header.tsx
@@ -1,20 +1,30 @@
 import Image from "next/image";
 import React from "react";
 import styles from "./Header.module.scss";
+import Link from "next/link";
 
 export const Header = () => {
   return (
     <>
-    <div className={styles.Header}>
-      <Image
-        className={styles.Header2}
-        src="/Workwiz_top.svg"
-        alt="Workwiz Logo"
-        width={250}
-        height={40}
-        priority
+      <div className={styles.Header}>
+        <Image
+          className={styles.Header_WorkwizTop}
+          src="/Workwiz_top.svg"
+          alt="Workwiz Logo"
+          width={250}
+          height={40}
+          priority
         />
-    </div>
+        <div className={styles.Header_right}>
+          <div className={styles.Header_right_item}>
+            <Link href="/">Home</Link>
+            <Link href="/blog">Blog</Link>
+            <Link href="/skill">Skill</Link>
+            <Link href="/contact">Contact</Link>
+          </div>
+        </div>
+      </div>
+      <hr className={styles.Header_hr} />
     </>
   );
 };
