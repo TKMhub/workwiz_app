151a0101cacad85f44cc07c2d9163660811d8d03
TKMhub <111867865+TKMhub@users.noreply.github.com>
2023-04-26 09:01:31 +0900

    0426_ofiice

--- components/Layout/Header/Header.tsx
+++ components/Layout/Header/Header.tsx
@@ -1,16 +1,20 @@
 import Image from "next/image";
 import React from "react";
+import styles from "./Header.module.scss";
 
 export const Header = () => {
   return (
     <>
+    <div className={styles.Header}>
       <Image
+        className={styles.Header2}
         src="/Workwiz_top.svg"
         alt="Workwiz Logo"
         width={250}
         height={40}
         priority
-      />
+        />
+    </div>
     </>
   );
 };
--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Header } from "components/Header/Header";
+import { Header } from "@/components/Layout/Header/Header";
 
 type Props = {
   children: React.ReactNode;
