a1e8f26f0c6b7cc1aebcc258951588e8cae60af0
TKMhub <111867865+TKMhub@users.noreply.github.com>
2023-04-28 00:01:01 +0900

    0427_home

--- components/Layout/Header/Header.tsx
+++ components/Layout/Header/Header.tsx
@@ -11,8 +11,8 @@ export const Header = () => {
           className={styles.Header_WorkwizTop}
           src="/Workwiz_top.svg"
           alt="Workwiz Logo"
-          width={250}
-          height={40}
+          width={170}
+          height={30}
           priority
         />
         <div className={styles.Header_right}>
@@ -25,6 +25,37 @@ export const Header = () => {
         </div>
       </div>
       <hr className={styles.Header_hr} />
+
+      ヘッダーがスクロール時に固定されるようにする
+      <style>
+        {`
+        .${styles.Header} {
+          position: fixed;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 60px;
+          background-color: #fff;
+          z-index: 100;
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          padding: 0 20px;
+          box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12);
+        }
+        .${styles.Header_WorkwizTop} {
+          cursor: pointer;
+        }
+        .${styles.Header_right} {
+          display: flex;
+          align-items: center;
+        }
+        .${styles.Header_right_item} {
+          display: flex;
+          align-items: center;
+          
+        `}
+      </style>
     </>
   );
 };
