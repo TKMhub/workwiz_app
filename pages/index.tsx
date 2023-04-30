38c0a605d84ce847322d532cbe3f21f5650ba893
TKMhub <111867865+TKMhub@users.noreply.github.com>
2023-04-30 21:55:49 +0900

    0430_home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,14 @@
 import styles from "./Home.module.scss";
 import Layout from "../components/Layout/Layout";
 import Link from "next/link";
+import Top from "@/components/Layout/Top/Top";
 
 export default function Home() {
   return (
     <>
       <Layout>
         <main className={styles.Home}>
-          <div>メインコンテンツー</div>
-          <div>メインコンテンツー</div>
-          <div>メインコンテンツー</div>
-          <div>メインコンテンツー</div>
-          <div>メインコンテンツー</div>
+        <Top />
         </main>
       </Layout>
     </>
