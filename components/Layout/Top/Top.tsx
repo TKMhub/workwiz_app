6d3600ff06a78879fcaaec7744792bf381573c98
TKMhub <111867865+TKMhub@users.noreply.github.com>
2023-05-01 22:57:06 +0900

    Top_err

--- components/Layout/Top/Top.tsx
+++ components/Layout/Top/Top.tsx
@@ -1,12 +1,6 @@
 import React from "react";
 import styles from "./Top.module.scss";
 import Image from "next/image";
-import NEXT from "@/public/nextjs-13.svg";
-import REACT from "@/public/react-2.svg";
-import DJANGO from "@/public/django-community.svg";
-import MATERIAL from "@/public/material-ui-1.svg";
-import SASS from "@/public/sass-1.svg";
-import VERCEL from "@/public/vercel.svg";
 
 const Top = () => {
   return (
@@ -26,8 +20,6 @@ const Top = () => {
               />
               は、高速で革新的なウェブアプリケーションを作成するための最先端のフレームワークです。サーバーサイドレンダリング(SSR)や静的サイト生成(SSG)が容易に実現でき、高いパフォーマンスとSEO対策が期待できます。また、APIルーティングやコード分割などの機能により、機能性と柔軟性が向上します。
             </p>
-            <br />
-            <br />
             <p>
               <Image
                 src="/react-2.svg"
@@ -39,7 +31,8 @@ const Top = () => {
               (React)は、Facebookが開発した人気のJavaScriptライブラリで、コンポーネントベースの開発を実現します。開発効率の向上と保守性の高いコードを提供し、アプリケーションの拡張性が向上します。さらに、Virtual
               DOMを利用した高速な描画性能を提供し、パフォーマンスも最適化されます。
             </p>
-            <br />
+          </div>
+          <br />
           <div className={styles.Top_TechSelection_style}>
             <h2>スタイル</h2>
             <br />
@@ -51,10 +44,9 @@ const Top = () => {
                 height={50}
                 priority
               />
-              (Material UI)は、Googleのマテリアルデザインを実装したReactのUIコンポーネントライブラリです。美しく使いやすいUIを短期間で開発でき、ユーザーエクスペリエンスの向上に貢献します。また、豊富なコンポーネントやテーマカスタマイズ機能を利用でき、ブランドイメージに合ったデザインが実現可能です。
+              (Material
+              UI)は、Googleのマテリアルデザインを実装したReactのUIコンポーネントライブラリです。美しく使いやすいUIを短期間で開発でき、ユーザーエクスペリエンスの向上に貢献します。また、豊富なコンポーネントやテーマカスタマイズ機能を利用でき、ブランドイメージに合ったデザインが実現可能です。
             </p>
-            <br />
-            <br />
             <p>
               <Image
                 src="/sass-1.svg"
@@ -66,9 +58,8 @@ const Top = () => {
               は、CSSの拡張言語で、変数やネスト機能などを使用できます。効率的なスタイリングを行い、コードの管理やメンテナンスが容易になります。また、Sassのミックスインやパーシャル機能により、コードの再利用性が高まり、開発速度が向上します。
             </p>
           </div>
-            <br />
-            <br />
-          </div>
+          <br />
+          <br />
           <div className={styles.Top_TechSelection_back}>
             <h2>バックエンド</h2>
             <p>
@@ -82,9 +73,6 @@ const Top = () => {
               は、Pythonで書かれた強力なWebフレームワークで、セキュリティ対策やデータベース処理が充実しています。開発速度の向上と堅牢なアプリケーションの構築が可能です。さらに、豊富な機能を持つ多くのサードパーティ製アプリケーションが利用でき、独自の機能拡張が容易です。
             </p>
           </div>
-          <br />
-          <br />
-
           <div className={styles.Top_TechSelection_host}>
             <h2>ホスティングサービス</h2>
             <p>
