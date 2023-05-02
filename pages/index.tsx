4d1b5091bd502a99a5ea380647aaac339df2c73c
TKMhub <111867865+TKMhub@users.noreply.github.com>
2023-05-02 13:02:35 +0900

    Top_footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,9 +8,78 @@ export default function Home() {
     <>
       <Layout>
         <main className={styles.Home}>
-        <Top />
+          <Top />
         </main>
       </Layout>
+      <footer>
+        <div className={styles.Footer}>
+          <div className={styles.Footer_Inner}>
+            <div className={styles.Footer_Inner_Box}>
+              <div className={styles.Footer_Inner_Box_Title}>
+                <h3>会社概要</h3>
+              </div>
+              <div className={styles.Footer_Inner_Box_Content}>
+                <p>会社名：株式会社○○○○</p>
+
+                <p>所在地：〒000-0000 東京都��
+
+                  <br />
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      {/* <style>
+        {`
+        // footerのスタイルを整えて。
+        .${styles.Footer} {
+          background-color: #002A76;
+          color: #fff;
+          padding: 40px 0;
+        }
+        .${styles.Footer_Inner} {
+          width: 100%;
+          max-width: 1280px;
+          margin: 0 auto;
+          display: flex;
+          justify-content: space-between;
+        }
+        .${styles.Footer_Inner_Box} {
+          width: 30%;
+        }
+        .${styles.Footer_Inner_Box_Title} {
+          margin-bottom: 20px;
+        }
+        .${styles.Footer_Inner_Box_Title} > h3 {
+          font-size: 18px;
+          font-weight: 600;
+          border-bottom: 2px solid #fff;
+          padding-bottom: 10px;
+        }
+        .${styles.Footer_Inner_Box_Content} {
+          font-size: 14px;
+        }
+        .${styles.Footer_Inner_Box_Content} > p {
+          margin-bottom: 10px;
+        }
+        .${styles.Footer_Inner_Box_Content} > p > span {
+          font-weight: 600;
+        }
+        .${styles.Footer_Inner_Box_Content} > p > br {
+          display: none;
+        }
+        @media (max-width: 768px) {
+          .${styles.Footer_Inner} {
+            flex-direction: column;
+          }
+          .${styles.Footer_Inner_Box} {
+            width: 100%;
+            margin-bottom: 40px;
+          }
+        }
+        `}
+      </style> */}
     </>
   );
 }
