75a3fc187cf9cc3451963b9432a2bdd20c0ca1a4
TKMhub <111867865+TKMhub@users.noreply.github.com>
2023-05-04 17:57:56 +0900

    Blog_commingSoon

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,11 +1,23 @@
 import Footer from '@/components/Footer'
 import Layout from '@/components/Layout'
+import styles from './Blog.module.scss'
 import React from 'react'
 
 const blog = () => {
   return (<>
     <Layout>
-    <div>blog</div>
+    <div className={styles.centerImage}>
+          <div className={styles.contentsBox}>
+            <div className={styles.pulseWrapper}>
+              <div className={styles.pulseDot1}></div>
+              <div className={styles.pulseDot2}></div>
+              <div className={styles.pulseDot3}></div>
+            </div>
+          </div>
+          <div className={styles.comming}>
+            <h1>Comming soon...</h1>
+          </div>
+        </div>
     </Layout>
     <Footer />
   </>
