0f7cc54e5183711496c9deb935f7ef1bd49e4ed4
TKMhub <111867865+TKMhub@users.noreply.github.com>
2023-05-17 12:51:31 +0900

    loginAfter_icon

--- components/HeaderLoginAfter/index.tsx
+++ components/HeaderLoginAfter/index.tsx
@@ -3,8 +3,11 @@ import React from "react";
 import styles from "./Header.module.scss";
 import Link from "next/link";
 import Button from "@mui/material/Button";
+import BrightnessLowIcon from '@mui/icons-material/BrightnessLow';
 
 export const Header = () => {
+  // プロップスの受け渡しでログイン時に利用したユーザーIDを取得する
+  const userId = "ユーザーＩＤ";
   return (
     <>
       <div className={styles.Header}>
@@ -20,18 +23,7 @@ export const Header = () => {
         </Link>
         <div className={styles.Header_right}>
           <div className={styles.Header_right_item}>
-            <Link href="/" className={styles.Header_right_item_link}>
-              ログイン完了
-            </Link>
-            <Link href="/blog" className={styles.Header_right_item_link}>
-              Blog
-            </Link>
-            <Link href="/skills" className={styles.Header_right_item_link}>
-              Skills
-            </Link>
-            <Link href="/contact" className={styles.Header_right_item_link}>
-              Contact
-            </Link>
+            <p>{userId}</p>
             <Link
               href="/login"
               className={styles.Header_right_item_buttonSpace}
@@ -41,21 +33,10 @@ export const Header = () => {
                 variant="contained"
                 style={{ width: '80px', height: '40px' }}
               >
-                Login
+                Logout
               </Button>
             </Link>
-            <Link
-              href="/login/signUp"
-              className={styles.Header_right_item_buttonSpace}
-            >
-              <Button
-                className={styles.Header_right_item_buttonSpace_button}
-                variant="contained"
-                style={{ width: '80px', height: '40px' }}
-              >
-                新規
-              </Button>
-            </Link>
+            <BrightnessLowIcon />
           </div>
         </div>
       </div>
